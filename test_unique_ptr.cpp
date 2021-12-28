71b543beb6eccab3d8407b1d13266702ef4587f4
InterviewNuts <96710519+InterviewNuts@users.noreply.github.com>
2021-12-28 19:11:39 +0530

    Unique pointer returned by value mean std::move()

--- test_unique_ptr.cpp
+++ test_unique_ptr.cpp
@@ -25,6 +25,10 @@ void test_unique_ptr::bark()
 unique_ptr<Dog> test_unique_ptr::testFun()
 {
      unique_ptr<Dog> dg(new Dog());
+	//! here since the return type is  by value , then by default it is like std::move();
+	//! the same is not true while receiving the unique pointe as an argument.
+	//! to receive it as an argument, then we need to pass the unique pointer as std::move(up);
+	//! return dg;
      return std::move(dg);
 }
 
