8afd0db526bf7eea5d183d60bcef37550ecd2b74
InterviewNuts <96710519+InterviewNuts@users.noreply.github.com>
2022-01-01 20:33:56 +0530

    Update Bit added

--- bit_manupulation/getBit_setBit_using_shift_operator.cpp
+++ bit_manupulation/getBit_setBit_using_shift_operator.cpp
@@ -26,9 +26,40 @@ void clearBit(int num, int pos)
 
     cout<<" The original number afetr bit cleared  = "<<num<<"\n";
 
-
-
 }
+
+void updateBit(int num, int pos)
+{
+    cout<<" The original number = "<<num<<"\n";
+    cout<<" 0 1 0 1 ( after clear the 2nd bit \n";
+    cout<<" 0 1 1 1 (so the value will turned to be 1) \n";
+
+    int mask=~(1<<pos);
+    num=num&(mask);
+    num=num|(1<<pos);
+
+    cout<<" The original number afetr bit cleared  = "<<num<<"\n";
+}
+
+void updateBitWithUserGivenValue(int num, int pos,int value)
+{
+    cout<<" The original number = "<<num<<"\n";
+    cout<<" 0 1 0 1 ( after clear the 2nd bit \n";
+    cout<<" 0 1 1 1 (so the value will turned to be 1) \n";
+
+    int mask=~(1<<pos);
+    cout<<" Mask = "<<mask<<"\n";
+    num=num&(mask);
+    cout<<" num = "<<num<<"\n";
+    cout<<" value = "<<value<<pos<<"\n";
+    num=num|(value<<pos);
+    cout<<" num2 = "<<num<<"\n";
+    cout<<" The original number afetr bit updated  = "<<num<<"\n";
+}
+
+
+
+
 
 int main()
 {
@@ -36,5 +67,7 @@ int main()
     cout<<setBit(5,1)<<"\n";
 
     clearBit(5,2);
+    updateBit(5,1);
+    updateBitWithUserGivenValue(4,2,0);
 }
 
