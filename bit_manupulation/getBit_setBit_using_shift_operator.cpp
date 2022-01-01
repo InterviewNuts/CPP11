897e8b21f8dbf47a098f13b4bd70dd05fdd03987
InterviewNuts <96710519+InterviewNuts@users.noreply.github.com>
2022-01-01 20:13:24 +0530

    clearBit() function added

--- bit_manupulation/getBit_setBit_using_shift_operator.cpp
+++ bit_manupulation/getBit_setBit_using_shift_operator.cpp
@@ -16,10 +16,25 @@ int  setBit(int num, int pos)
     
 }
 
+void clearBit(int num, int pos)
+{
+    cout<<" The original number = "<<num<<"\n";
+    cout<<" 0 1 0 1 ( after clear the 3rd bit \n";
+    cout<<" 0 0 0 1 (so the value will turned to be 1) \n";
+
+    num=num&(~(1<<pos));
+
+    cout<<" The original number afetr bit cleared  = "<<num<<"\n";
+
+
+
+}
 
 int main()
 {
     cout<<getBit(4,2)<<"\n";
     cout<<setBit(5,1)<<"\n";
+
+    clearBit(5,2);
 }
 
