ea9a84d8c779a75c69b6e9187f6448e17fef3031
Haramohan Sahu <96710519+InterviewNuts@users.noreply.github.com>
2022-01-03 18:50:25 +0530

    Update hello_world_with_threads.cpp

--- threads/hello_world_with_threads.cpp
+++ threads/hello_world_with_threads.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file hello_world_with_threads.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <iostream>
 #include <thread>
 #include <chrono>
