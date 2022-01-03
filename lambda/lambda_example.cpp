d3e2189d94965318358dcb364fa98d0f276bc0b7
Haramohan Sahu <96710519+InterviewNuts@users.noreply.github.com>
2022-01-03 16:21:15 +0530

    Update lambda_example.cpp

--- lambda/lambda_example.cpp
+++ lambda/lambda_example.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file lambda_example.cpp
+ * @author Haramohan Sahu (hara.sahu@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
 #include<iostream>
 #include<string>
