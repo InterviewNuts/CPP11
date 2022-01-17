cd0d5d8caa5a26455cd81db8ba3db49c810f7319
InterviewNuts <96710519+InterviewNuts@users.noreply.github.com>
2022-01-18 04:53:33 +0530

    comment added

--- lock/mutex_with_try_lock.cpp
+++ lock/mutex_with_try_lock.cpp
@@ -1,3 +1,9 @@
+// 0. try_lock() Tries to lock the mutex. Returns immediately. On successful lock acquisition returns true otherwise returns false.
+// 1. If try_lock() is not able to lock mutex, then it doesn't get blocked that's why it is called non-blocking.
+// 2. If try_lock is called again by the same thread which owns the mutex, the behavior is undefined.
+//    It is a dead lock situation with undefined behaviour. (if you want to be able to lock the same mutex by same thread
+//    more than one time the go for recursive_mutex)
+
 #include <iostream>
 #include <thread>
 // #include<future>
